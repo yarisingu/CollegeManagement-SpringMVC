fb4eb4703132d23fe63c1a01a4b7d51ac1e5233a
YARISINGU TARUN <118651530+yarisingu@users.noreply.github.com>
2023-06-14 14:13:45 +0000

    re writing

--- src/main/java/com/tarun/collegesoft/controller/StaffController.java
+++ src/main/java/com/tarun/collegesoft/controller/StaffController.java
@@ -1,13 +1,7 @@
 package com.tarun.collegesoft.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.tarun.collegesoft.dto.Staff;
 import com.tarun.collegesoft.service.StaffService;
 
 
